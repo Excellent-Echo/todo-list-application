6e9fe6671a4b917ff05115ae0cb63e07ec62dc7a
sonex23 <sonaccx7@gmail.com>
2021-04-05 01:50:06 +0700

    All done

--- src/components/RightContent/index.jsx
+++ src/components/RightContent/index.jsx
@@ -2,36 +2,68 @@ import React, { useState } from "react";
 import { Modal, Button } from "react-bootstrap";
 import ButtonAdd from "../ButtonAdd";
 import "./index.css";
-import Task from "../Task";
+import ListTasks from "../ListTasks";
 
 const RightContent = () => {
-  const [showModal, setShow] = useState(false);
 
+  //modal hooks
+  const [showModal, setShow] = useState(false);
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
+
+  const [taskTitle, setTaskTitle] = useState("");
+  const [taskDesc, setTaskDesc] = useState("");
+  const [taskId, setTaskId] = useState(0);
+  const [listTasks, setListTasks] = useState([]);
+
+
+  const handleForm = e =>{
+      e.preventDefault();
+
+      setListTasks([
+        ...listTasks, 
+        {
+          title : taskTitle,
+          desc:taskDesc,
+          complete:false, 
+          id : taskId
+        }
+      ]);
+
+      setTaskId(taskId + 1);
+      setTaskTitle("");
+      setTaskDesc("");
+      handleClose()
+  }
+
+  const deleteTask = (id)=>{
+    const newListTasks = listTasks.filter((task)=>task.id !== id);
+    setListTasks(newListTasks);
+  } 
+
+  const changeStatus = (id)=>{
+    const newListTask = listTasks.map((task)=>task.id === id?{...task, complete:true}:task);
+    setListTasks(newListTask);
+  }
+  
   return (
     <div className="right-container">
       <div className="right-header">
         <ButtonAdd handleButton={handleShow}/>
       </div>
-      <div className="right-content">
-        <Task />
-        <Task />
-        <Task />
-        <Task />
-        <Task />
-      </div>
+      
+      <ListTasks listTasks={listTasks} deleteTask={deleteTask} changeStatus={changeStatus}/>
 
       <Modal show={showModal} onHide={handleClose} size="md">
         <Modal.Header>
           <Modal.Title>Add New Task</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-            <form>
+            <form onSubmit={handleForm}>
                 <label htmlFor="title">Title</label>
-                <input type="text" name="title" id="title" className="form-control mb-3"/>
+                <input type="text" name="title" id="title" className="form-control mb-3" onChange={e=>{setTaskTitle(e.target.value)}}/>
                 <label htmlFor="desc">Description</label>
-                <textarea name="desc" id="desc" className="form-control mb-3"/>
+                <textarea name="desc" id="desc" className="form-control mb-3" onChange={e=>{setTaskDesc(e.target.value)}}/>
                 <input type="submit" value="Submit" className="btn btn-success form-control"/>
             </form>
         </Modal.Body>
@@ -44,5 +76,6 @@ const RightContent = () => {
     </div>
   );
 };
+
 
 export default RightContent;
--- src/components/Task/index.js
+++ src/components/Task/index.js
@@ -1,13 +1,40 @@
 import React from "react";
 import "./index.css";
 const Task = ({ task, deleteTask, changeStatus }) => {
+  const cekStatus = () => {
+    if (task.complete === false) {
+      return (
+        <button
+          className="btn btn-success w-25 mx-1"
+          onClick={() => changeStatus(task.id)}
+        >
+          Complete
+        </button>
+      );
+    } else {
+      return (
+        <button
+          className="btn btn-outline-success w-50 mx-1 disabled"
+          onClick={() => changeStatus(task.id)}
+        >
+          Task is Completed !
+        </button>
+      );
+    }
+  };
+
   return (
     <div className="card shadow-lg task-container p-3 mb-4">
-      <h4>Task Title</h4>
-      <p>Task Description</p>
+      <h4>{task.title}</h4>
+      <p>{task.desc}</p>
       <div className="btn-task">
-        <button className="btn btn-success w-25 mx-1">Complete</button>
-        <button className="btn btn-danger w-25 mx-1">Remove</button>
+        {cekStatus()}
+        <button
+          className="btn btn-danger w-25 mx-1"
+          onClick={() => deleteTask(task.id)}
+        >
+          Remove
+        </button>
       </div>
     </div>
   );
