e0b6fdbff933e01f8318e9b001bc7f5a4f2f0a8e
marwanjuna <mrwnolii@gmail.com>
2021-04-21 22:27:30 +0700

    change icon

--- src/components/ToDoForm.jsx
+++ src/components/ToDoForm.jsx
@@ -76,28 +76,6 @@ const ToDoForm = ({ addTask }) => {
           </button>
         </div>
       </form>
-      {/* <div className="container mt-3 categories">
-        <div className="row">
-          <div className="col-sm-12 d-flex justify-content-center">
-            <div id="personal" className="category">
-              <p>Personal</p>
-              <p>{personalTask} task(s)</p>
-            </div>
-            <div id="work" className="category">
-              <p>Work</p>
-              <p>{workTask} task(s)</p>
-            </div>
-            <div id="health" className="category">
-              <p>Health &amp; Fit</p>
-              <p>{healthTask} task(s)</p>
-            </div>
-            <div id="shopping" className="category">
-              <p>Shopping</p>
-              <p>{shoppingTask} task(s)</p>
-            </div>
-          </div>
-        </div>
-      </div> */}
       <DisplayCategories 
         personalTask={personalTask}
         workTask={workTask}
